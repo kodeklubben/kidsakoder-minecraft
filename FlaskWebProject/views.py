0f78ec4b6cf7e395f2a06bd6e7df7b8dd532b318
DeimosA <krvolden@gmail.com>
2016-02-17 11:00:42 +0100

    Azure Flask skeleton files

---
+++ FlaskWebProject/views.py
@@ -0,0 +1,36 @@
+"""
+Routes and views for the flask application.
+"""
+
+from datetime import datetime
+from flask import render_template
+from FlaskWebProject import app
+
+@app.route('/')
+def home():
+    """Renders the home page."""
+    return render_template(
+        'index.html',
+        title='Home',
+        year=datetime.now().year,
+    )
+
+@app.route('/contact')
+def contact():
+    """Renders the contact page."""
+    return render_template(
+        'contact.html',
+        title='Contact',
+        year=datetime.now().year,
+        message='Your contact page.'
+    )
+
+@app.route('/about')
+def about():
+    """Renders the about page."""
+    return render_template(
+        'about.html',
+        title='About',
+        year=datetime.now().year,
+        message='Your application description page.'
+    )
