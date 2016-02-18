a10f4fa652f1db949951436a295e17ee5f8388cd
DeimosA <krvolden@gmail.com>
2016-02-18 23:38:21 +0100

    Session test
    Redirect to login if no session

--- flask_app/views.py
+++ flask_app/views.py
@@ -3,13 +3,20 @@ Routes and views for the flask application.
 """
 
 from datetime import datetime
-from flask import render_template, request, session
+from flask import render_template, request, session, escape, redirect, url_for
 from flask_app import app
 import var_file
+
+app.secret_key = var_file.secret_key
 
 @app.route('/')
 def home():
     """Renders the home page."""
+    if 'username' in session:
+        return 'jalla ' + escape(session['username'])
+    else:
+        return redirect(url_for('login'))
+    
     return render_template(
         'index.html',
         title='Hjem',
@@ -32,8 +39,11 @@ def contact():
 def login():
     """ Login page """
     if request.method == 'POST':
-        processLogin()
+        session['username'] = request.form.get('username')
+        return redirect(url_for('home'))
+        #processLogin()
     else:
+        
         return render_template(
             'login.html',
             title = 'Logg inn',
