b0543e423c916ee4f347be5c38819fea3873d356
Andreas Dørum <andreas.doerum@gmail.com>
2016-02-19 14:59:12 +0100

    add schema.sql with basic map and meeting schemas

---
+++ flask_app/schema.sql
@@ -0,0 +1,11 @@
+DROP TABLE if EXISTS maps; /* Reset database on creation */
+CREATE TABLE maps (
+  id integer PRIMARY KEY AUTOINCREMENT,
+  file_reference text NOT NULL /* reference to location in filesystem */
+);
+CREATE TABLE meetings (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  time TEXT NOT NULL, /* YYYY-MM-DD HH:MM:SS.SSS */
+  map_id INTEGER NOT NULL,
+  FOREIGN KEY(map_id) REFERENCES maps(id)
+);
