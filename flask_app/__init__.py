22cb1b613d160f70680e62977409fdd8c27b4bb7
Andreas Dørum <andreas.doerum@gmail.com>
2016-02-20 14:13:08 +0100

    add config-file to gitignore and move settings to config.

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -4,11 +4,9 @@ The flask application package.
 
 from flask import Flask
 
-DATABASE = 'C:\\Users\\Andreas\\dev\\kidsakoder-minecraft\\flask_app\\tmp\\database.db'
-SECRET_KEY = 'development key'
 DEBUG = True
 app = Flask(__name__)
-app.config.from_object(__name__)
+app.config.from_envvar('APP_SETTINGS', silent=False)
 
 
 import flask_app.database
