eb67bf5fb272b9134ddbe9981c6652013ecab250
Andreas Dørum <andreas.doerum@gmail.com>
2016-02-20 14:40:13 +0100

    add basic user support to database.

--- flask_app/schema.sql
+++ flask_app/schema.sql
@@ -8,9 +8,18 @@ CREATE TABLE meetings (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   title TEXT NOT NULL,
   time TEXT NOT NULL, /* YYYY-MM-DD HH:MM:SS.SSS */
+  creator TEXT NOT NULL,
   map_id INTEGER,
+  FOREIGN KEY(creator) REFERENCES users(username),
   FOREIGN KEY(map_id) REFERENCES maps(id)
 );
+DROP TABLE if EXISTS users;
+CREATE TABLE users (
+  username TEXT PRIMARY KEY,
+  password TEXT NOT NULL
+);
+
 INSERT INTO maps (file_reference) VALUES ('maps/mymap');
-INSERT INTO meetings (title, time, map_id) VALUES ('Kodeklubb-møte', '1992-12-18 15:10:20.101', 1);
-INSERT INTO meetings (title, time, map_id) VALUES ('Kløfta møte', '2000-12-18 15:10:20.101', 1);
+INSERT INTO users (username, password) VALUES ('kari', 'passord');
+INSERT INTO meetings (title, time, creator, map_id) VALUES ('Kodeklubb-møte', '1992-12-18 15:10:20.101', 'kari', 1);
+INSERT INTO meetings (title, time, creator, map_id) VALUES ('Kløfta møte', '2000-12-18 15:10:20.101', 'kari', 1);
