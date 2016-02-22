daa028d9b63c85529425061025d382f1fce4f673
DeimosA <krvolden@gmail.com>
2016-02-22 14:57:35 +0100

    added User methods

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -4,12 +4,16 @@ The flask application package.
 
 from flask import Flask
 from flask.ext import login
+import models
 
 app = Flask(__name__)
 
 # Flask-Login initialization
 login_manager = login.LoginManager()
 login_manager.init_app(app)
+@login_manager.user_user_loader
+def load_user(user_id):
+    return models.User.get(user_id)
 
 # See configuration.py for possible configuration objects
 app.config.from_object('flask_app.configuration.Development')
--- flask_app/models.py
+++ flask_app/models.py
@@ -1,18 +1,33 @@
-from werkzeug.security import generate_password_hash, \
-     check_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(object):
 
     def __init__(self, username, password):
         self.username = username
         self.set_password(password)
+    
+    def get(cls, username):
+        # Get user details from db
+        # Return None if invalid user
+        password = 'jalla'
+        return cls(username, password)
 
     def set_password(self, password):
         self.pw_hash = generate_password_hash(password, method='pbkdf2:sha512:100000', salt_length=32)
 
     def check_password(self, password):
         return check_password_hash(self.pw_hash, password)
-        
+    
+    # Flask-Login methods:
+    def is_authenticated():
+        return True
+    def is_active():
+        return True
+    def is_anonymous():
+        return False
+    def get_id():
+        # Must return Unicode string
+        return self.username
 
 #Hot to test:        
 # me = User('yourusername', 'password12345678987654321')
