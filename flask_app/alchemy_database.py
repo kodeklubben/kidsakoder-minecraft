982183c24a2e4c0cffb1d352ff32525c6c106317
Andreas Dørum <andreas.doerum@gmail.com>
2016-02-24 11:45:17 +0100

    add init methods to alchemy db setup, and link to config file

--- flask_app/alchemy_database.py
+++ flask_app/alchemy_database.py
@@ -1,9 +1,27 @@
 from sqlalchemy import create_engine
-from sqlalchemy.dialects import sqlite
 
+from flask.ext.app import app
 from flask.ext.app.alchemy_schema import metadata
+
+engine = create_engine(app.config['DATABASE_ALCHEMY'], echo=True)
+
+
+def connect_db():
+    return engine.connect()
 
 
 def init_db():
-    engine = create_engine('sqlite:\\\\\\:memory:', echo=True)
-    metadata.create_all(engine)
+    metadata.drop_all(engine)  # Empties the database
+    metadata.create_all(engine)  # Creates all tables from metadata definition
+
+
+@app.before_request
+def before_request():
+    g.db = connect_db()
+
+
+@app.teardown_request
+def teardown_request(exception):
+    db = getattr(g, 'db', None)
+    if db is not None:
+        db.close()
