95125a6773bf0c1cef65b97c74be42adfdbda0c4
eivind2610 <eivingj@stud.ntnu.no>
2016-02-24 12:45:39 +0100

    404 template and route, closes #58 #59
    Added a 404 template, and a route that leads to it whenever an
    unrecognized URL is used.

--- flask_app/views.py
+++ flask_app/views.py
@@ -100,3 +100,7 @@ def add_meeting():
     g.db.commit()
     flash('Nytt mote lagt til!')
     return redirect(url_for('database'))
+    
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
