9267c160d4274bd6b42c9e80ccd998e28ebad4b2
Andreas Dørum <andreas.doerum@gmail.com>
2016-02-25 09:26:44 +0100

    add admin user when init db

--- flask_app/database.py
+++ flask_app/database.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-from flask_app import app
 from flask.ext.app.schema import metadata
+from flask_app import app
 
 engine = create_engine(app.config['DATABASE'], echo=True, convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
@@ -15,6 +15,9 @@ Base.query = db_session.query_property()
 
 
 def init_db():
-    import flask_app.models
+    from flask.ext.app import models
     metadata.drop_all(engine)  # Empties the database
     Base.metadata.create_all(bind=engine)  # Creates all tables from metadata definition
+    admin = models.User('admin', 'adminpass1234', 'Administrator', 'admin@mail.com', False)
+    db_session.add(admin)
+    db_session.commit()
