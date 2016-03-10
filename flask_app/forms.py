83576efe2e2313cf6a057af716ebc7b26ffe6b9a
DeimosA <krvolden@gmail.com>
2016-03-10 19:20:33 +0100

    Integer participant count

--- flask_app/forms.py
+++ flask_app/forms.py
@@ -3,7 +3,7 @@ Flask-WTF form controller
 """
 
 from flask_wtf import Form
-from wtforms import StringField, DateTimeField
+from wtforms import StringField, DateTimeField, IntegerField
 from wtforms.validators import DataRequired, Length
 
 
@@ -11,5 +11,5 @@ class MeetingForm(Form):
     title = StringField('Navn', [Length(min=4, max=25)])
     start_time = DateTimeField('Starttidspunkt')
     end_time = DateTimeField('Sluttidspunkt')
-    participant_count = StringField('Antall deltakere')
+    participant_count = IntegerField('Antall deltakere')
     world_ref = StringField('Minecraft verden')
