08bd4baa6c6f59e4f5e557f770d2782d19d62b77
Birger <blautkake@dhcp-080212.wlan.ntnu.no>
2016-04-04 17:13:51 +0200

    Change dateFields to datePickers

--- flask_app/forms.py
+++ flask_app/forms.py
@@ -3,13 +3,14 @@ Flask-WTF form controller
 """
 
 from flask_wtf import Form
-from wtforms import StringField, DateTimeField, IntegerField
+from wtforms import StringField, IntegerField
+from wtforms.fields.html5 import DateField
 from wtforms.validators import DataRequired, Length
 
 
 class MeetingForm(Form):
     title = StringField('Navn', [Length(min=4, max=25)])
-    start_time = DateTimeField('Starttidspunkt')
-    end_time = DateTimeField('Sluttidspunkt')
+    start_time = DateField('Starttidspunkt', format='%Y-%m-%d')
+    end_time = DateField('Sluttidspunkt', format='%Y-%m-%d')
     participant_count = IntegerField('Antall deltakere')
     world_ref = StringField('Minecraft verden')
