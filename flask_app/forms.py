0cf7347d49158d3e8ab4b273b5eb50a4b77fca1c
Birger <birgerto@stub.ntnu.no>
2016-04-18 17:16:44 +0200

    apply bootstrap datetimepicker to datefields

--- flask_app/forms.py
+++ flask_app/forms.py
@@ -3,14 +3,13 @@ Flask-WTF form controller
 """
 
 from flask_wtf import Form
-from wtforms import StringField, IntegerField, HiddenField
-from wtforms.fields.html5 import DateField
+from wtforms import StringField, IntegerField, HiddenField, DateTimeField
 from wtforms.validators import DataRequired, Length
 
 
 class MeetingForm(Form):
     title = StringField('Navn', [Length(min=4, max=25)])
-    start_time = DateField('Starttidspunkt', format='%Y-%m-%d')
-    end_time = DateField('Sluttidspunkt', format='%Y-%m-%d')
+    start_time = DateTimeField('Starttidspunkt', format='%d.%m.%Y %H:%M')
+    end_time = DateTimeField('Sluttidspunkt', format='%d.%m.%Y %H:%M')
     participant_count = IntegerField('Antall deltakere')
     world_ref = HiddenField('Minecraft verden')
