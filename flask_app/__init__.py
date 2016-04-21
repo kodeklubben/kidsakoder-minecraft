e355967d4309e39192266c2981d70b888a5bd47b
DeimosA <krvolden@gmail.com>
2016-04-21 16:43:37 +0200

    Improve meeting list

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -18,6 +18,19 @@ app = Flask(__name__)
 # See configuration.py for possible configuration objects
 app.config.from_object('flask_app.configuration.Development')
 
+
+# Set locale for datetime format
+import locale
+try:
+    locale.setlocale(locale.LC_TIME, 'nb_NO')
+except locale.Error:
+    try:
+        locale.setlocale(locale.LC_TIME, 'norwegian-bokmal')
+    except locale.Error:
+        print 'Norwegian locale not found'
+
+
+
 # Initialize Flask-Security
 from database import db, roles_users
 from flask_security import Security, SQLAlchemyUserDatastore, UserMixin, RoleMixin
@@ -26,10 +39,11 @@ from models import User, Role, Meeting, World
 user_datastore = SQLAlchemyUserDatastore(db, User, Role)
 security = Security(app, user_datastore)
 
+
 # Initialize Flask-Admin and add needed views/pages
 admin = Admin(app)
 
-#Configurations for admin panel about meetings
+# Configurations for admin panel about meetings
 class MeetingView(ModelView):
     def is_accessible(self):
         return current_user.has_role('admin')
@@ -38,22 +52,22 @@ class WorldView(ModelView):
     def is_accessible(self):
         return current_user.has_role('admin')
 
-#Configurations to a view for displaying, deleting, adding and editing users.
+# Configurations to a view for displaying, deleting, adding and editing users.
 class UserView(ModelView):
     column_exclude_list = ['password', 'first_name', 'last_name']
     
-    #Automatically display readable roles
+    # Automatically display readable roles
     column_auto_select_related = True
     
     def index(self):
         self._admin_template_args['active'] = True
     
-    #Makes sure only admins can make changes to anything concerning users through the admin panel,
-    #by not displaying the User-tab if admin panel is somehow accessed by a regular user.
+    # Makes sure only admins can make changes to anything concerning users through the admin panel,
+    # by not displaying the User-tab if admin panel is somehow accessed by a regular user.
     def is_accessible(self):
         return current_user.has_role('admin')
     
-    #Replaces Flask's standard textfield for passwords with an actual password field
+    # Replaces Flask's standard textfield for passwords with an actual password field
     def scaffold_form(self):
         form_class = super(UserView, self).scaffold_form()
         form_class.password2 = PasswordField('Passord',[
@@ -62,16 +76,16 @@ class UserView(ModelView):
         form_class.confirm = PasswordField('Bekreft passordet')
         return form_class
     
-    #Makes sure the data from the new PW field is sent to the DB, so that the new PW field is an actual replacement, and
-    #not just a field that says "Password". Also hashes PW's before sending them to the DB. If PW field is blank in edit user,
-    #existing PW will be kept. Gives error message when PW field is blank in create - not sure how to fix.
+    # Makes sure the data from the new PW field is sent to the DB, so that the new PW field is an actual replacement, and
+    # not just a field that says "Password". Also hashes PW's before sending them to the DB. If PW field is blank in edit user,
+    # existing PW will be kept. Gives error message when PW field is blank in create - not sure how to fix.
     def on_model_change(self, form, model, is_created):
         if len(model.password2):
             model.password = utils.encrypt_password(model.password2)
     
-    #Excludes fields we don't want to display. Both fields that are not needed (names, confirmed_at), and fields that we
-    #want to avoid using, such as password and (soon) active. Separated on different lines so singular ones can be
-    #commented out, to check what causes errors.
+    # Excludes fields we don't want to display. Both fields that are not needed (names, confirmed_at), and fields that we
+    # want to avoid using, such as password and (soon) active. Separated on different lines so singular ones can be
+    # commented out, to check what causes errors.
     form_excluded_columns = [#'active',
     'confirmed_at',
     'first_name',
@@ -79,11 +93,10 @@ class UserView(ModelView):
     'password'
     ]
 
-#Adds previously configured views
+# Adds previously configured views
 admin.add_view(UserView(User, db.session))
 admin.add_view(MeetingView(Meeting, db.session))
 admin.add_view(WorldView(World, db.session))
-
 
 
 #from datetime import datetime
