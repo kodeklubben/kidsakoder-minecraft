db4a2d28952133236e96d30176299ddbb5b72c88
DeimosA <krvolden@gmail.com>
2016-05-08 17:55:55 +0200

    Translate login form labels

--- flask_app/config/configuration.py
+++ flask_app/config/configuration.py
@@ -23,8 +23,8 @@ SECURITY_DEFAULT_REMEMBER_ME = False
 
 SECURITY_MSG_DISABLED_ACCOUNT = (u'Denne kontoen er deaktivert', 'error')
 SECURITY_MSG_EMAIL_NOT_PROVIDED = (u'E-post adresse mangler', 'error')
-SECURITY_MSG_INVALID_PASSWORD = (u'Feil brukernavn og / eller passord', 'error')
+SECURITY_MSG_USER_DOES_NOT_EXIST = (u'Feil brukernavn og / eller passord', 'error')
+SECURITY_MSG_INVALID_PASSWORD = SECURITY_MSG_USER_DOES_NOT_EXIST
 SECURITY_MSG_LOGIN = (u'Venligst logg inn for å få tilgang til denne siden', 'info')
 SECURITY_MSG_PASSWORD_NOT_PROVIDED = (u'Passord mangler', 'error')
 SECURITY_MSG_UNAUTHORIZED = (u'Du har ikke tilgang til å se denne ressursen', 'error')
-SECURITY_MSG_USER_DOES_NOT_EXIST = (u'Feil brukernavn og / eller passord', 'error')
