3d2135602aacb47afe1aa327d57b1b498fd8753b
Andreas Dørum <andreas.doerum@gmail.com>
2016-05-11 15:19:59 +0200

    rename poll function. Tasks now sets own Status

--- flask_app/tasks.py
+++ flask_app/tasks.py
@@ -1,4 +1,5 @@
-from celery import Celery
+from celery import Celery, current_app
+from celery.signals import after_task_publish
 import subprocess
 
 app = Celery('tasks', broker='amqp://guest@master//')
@@ -7,11 +8,15 @@ app.conf.update(
     CELERY_TRACK_STARTED=True,
 )
 
-@app.task(name='tasks.generate_preview_task')
-def generate_preview_task(config_path):
+@app.task(name='tasks.generate_preview_task', bind=True)
+def generate_preview_task(self, config_path):
     # Call overviewer to generate
     # WINDOWS
     # subprocess.call(["C:\users\Andreas\overviewer\overviewer.exe", world_path, preview_path])
     # Linux
+    task_id = self.request.id # Get own id
+    backend = self.backend
+    backend.store_result(task_id, None, "SENT") # Set own status to SENT
     subprocess.call(["overviewer.py", "--config=%s" % config_path])
     return "Preview complete."
+
