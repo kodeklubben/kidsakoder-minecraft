ab897b874ce2fad7d3acef874b3a9aabd8611a24
Birger <birgerto@stub.ntnu.no>
2016-05-12 20:49:53 +0200

    add test for login and logout

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,16 +3,16 @@ import os
 
 from flask_app import app
 
+
 @pytest.fixture
 def client(request):
-    app.config['TESTING'] = True
     client = app.test_client()
-
+    app.config['WTF_CSRF_ENABLED'] = False
     return client
 
-def login(client, username, password):
+def login(client, email, password):
     return client.post('/login', data=dict(
-        username=username,
+        email=email,
         password=password
     ), follow_redirects=True)
 
@@ -21,7 +21,20 @@ def logout(client):
     return client.get('/logout', follow_redirects=True)
 
 
+
 def test_front_page(client):
     """Make sure we resieve an HTML page"""
     rv = client.get('/', follow_redirects=True)
     assert rv.status == '200 OK'
+
+
+def test_login_logout(client):
+    """Make sure login and logout works"""
+    rv = login(client, 'admin@mail.com', 'adminpass123')
+    assert 'Velg verden' in rv.data
+    rv = logout(client)
+    assert 'Venligst logg inn' in rv.data
+    rv = login(client, 'admin@mail.com' + 'x', 'adminpass123')
+    assert 'Feil brukernavn og / eller passord' in rv.data
+    rv = login(client, 'admin@mail.com', 'adminpass123' + 'x')
+    assert 'Feil brukernavn og / eller passord' in rv.data
