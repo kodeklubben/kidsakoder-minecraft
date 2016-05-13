8b241e4f1d3385587e1f6a16de6ec35d08bc285d
Birger <birgerto@stub.ntnu.no>
2016-05-13 12:24:04 +0200

    move stuff to config file

--- tests/test_app.py
+++ tests/test_app.py
@@ -7,7 +7,7 @@ from flask_app import app
 @pytest.fixture
 def client(request):
     client = app.test_client()
-    app.config['WTF_CSRF_ENABLED'] = False
+    app.config.from_pyfile('config/testing.py')
     return client
 
 def login(client, email, password):
@@ -30,20 +30,21 @@ def test_front_page(client):
 
 def test_login_logout(client):
     """Make sure login and logout works"""
-    rv = login(client, 'admin@mail.com', 'adminpass123')
+    rv = login(client, app.config['TEST_EMAIL'], app.config['TEST_PASSWORD'])
     assert 'Velg verden' in rv.data
     rv = logout(client)
-    assert 'Venligst logg inn' in rv.data
-    rv = login(client, 'admin@mail.com' + 'x', 'adminpass123')
-    assert 'Feil brukernavn og / eller passord' in rv.data
-    rv = login(client, 'admin@mail.com', 'adminpass123' + 'x')
-    assert 'Feil brukernavn og / eller passord' in rv.data
+    assert app.config['SECURITY_MSG_LOGIN'][0].encode('utf-8') in rv.data
+    rv = login(client, app.config['TEST_EMAIL'] + 'x', app.config['TEST_PASSWORD'])
+    assert app.config['SECURITY_MSG_USER_DOES_NOT_EXIST'][0] in rv.data
+    rv = login(client, app.config['TEST_EMAIL'], app.config['TEST_PASSWORD'] + 'x')
+    assert app.config['SECURITY_MSG_INVALID_PASSWORD'][0] in rv.data
 
 
 def test_access_before_after_login(client):
+    """Make sure we do not have access before login"""
     rv = client.get('/kontakt', follow_redirects=True)
     assert 'Kode-Kidza' not in rv.data
-    assert 'Venligst logg inn' in rv.data
-    login(client, 'admin@mail.com', 'adminpass123')
+    assert app.config['SECURITY_MSG_LOGIN'][0].encode('utf-8') in rv.data
+    login(client, app.config['TEST_EMAIL'], app.config['TEST_PASSWORD'])
     rv = client.get('/kontakt', follow_redirects=True)
     assert 'Kode-Kidza' in rv.data
