299fc01cdea6f344d86fac15b77196b777b5917b
Birger <birgerto@stub.ntnu.no>
2016-05-13 15:09:29 +0200

    make globals for test mail and password

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,16 +5,25 @@ from flask_app.models import User
 from flask_app.database import db, init_db
 
 
+EMAIL = ''
+PASSWORD = ''
+
+
 @pytest.fixture
 def client(request):
     """ Creates the test client """
     client = app.test_client()
     app.config.from_pyfile('config/testing.py')
 
+    global EMAIL
+    EMAIL = app.config['TEST_EMAIL']
+    global PASSWORD
+    PASSWORD = app.config['TEST_PASSWORD']
+
     """ Initialize db and add a test user """
     init_db()
-    user_datastore.create_user(email=app.config['TEST_EMAIL'], password=app.config['TEST_PASSWORD'])
-    user_datastore.add_role_to_user(app.config['TEST_EMAIL'], 'admin')
+    user_datastore.create_user(email=EMAIL, password=PASSWORD)
+    user_datastore.add_role_to_user(EMAIL, 'admin')
     db.session.commit()
 
     return client
@@ -41,17 +50,18 @@ def test_front_page(client):
 
 def test_login_logout(client):
     """Make sure login works and logout works """
-    rv = login(client, app.config['TEST_EMAIL'], app.config['TEST_PASSWORD'])
+    rv = login(client, EMAIL, PASSWORD)
+    print rv.data
     assert 'Velg verden' in rv.data
 
     rv = logout(client)
     assert app.config['SECURITY_MSG_LOGIN'][0].encode('utf-8') in rv.data
 
     """ Make sure we are denied access using incorrect credentials """
-    rv = login(client, app.config['TEST_EMAIL'] + 'x', app.config['TEST_PASSWORD'])
+    rv = login(client, EMAIL + 'x', PASSWORD)
     assert app.config['SECURITY_MSG_USER_DOES_NOT_EXIST'][0] in rv.data
 
-    rv = login(client, app.config['TEST_EMAIL'], app.config['TEST_PASSWORD'] + 'x')
+    rv = login(client, EMAIL, PASSWORD + 'x')
     assert app.config['SECURITY_MSG_INVALID_PASSWORD'][0] in rv.data
 
 
@@ -62,6 +72,6 @@ def test_access_before_after_login(client):
     assert app.config['SECURITY_MSG_LOGIN'][0].encode('utf-8') in rv.data
 
     """ Make sure we do have access after login """
-    login(client, app.config['TEST_EMAIL'], app.config['TEST_PASSWORD'])
+    login(client, EMAIL, PASSWORD)
     rv = client.get('/kontakt', follow_redirects=True)
     assert 'Kode-Kidza' in rv.data
