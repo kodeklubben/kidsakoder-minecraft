ddcbe80312c6aedc1aaf31a4542387a966f35aa5
Sandor Zeestraten <sandor@zeestrataca.com>
2016-05-23 15:05:42 +0200

    Cleaned up Azure management certificate in Terraform

--- terraform/master/install_master.sh
+++ terraform/master/install_master.sh
@@ -2,18 +2,17 @@
 
 # Create Salt configuration directory
 mkdir /etc/salt/
+
 # Copy Salt Master and Minion configuration
 cp /tmp/saltstack/etc/master.conf /etc/salt/master
 cp /tmp/saltstack/etc/master_minion.conf /etc/salt/minion
+
 # Copy Salt Pillars
 cp -r /tmp/saltstack/pillar/ /srv/pillar/
 # Copy Salt States
 cp -r /tmp/saltstack/salt/ /srv/salt/
 # Copy Salt Reactors
 cp -r /tmp/saltstack/reactor/ /srv/reactor/
-
-# Copy Azure .pem certificate for Salt Cloud
-cp /tmp/saltstack/etc/kidsakoder.pem /etc/salt/kidsakoder.pem
 
 # Download SaltStack bootstrap script
 wget -O /tmp/bootstrap-salt.sh https://bootstrap.saltstack.com
